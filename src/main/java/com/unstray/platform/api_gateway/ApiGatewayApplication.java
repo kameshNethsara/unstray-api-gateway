3645ed537d2ceb5c0f29157338e60eb84f44e6e8
kameshNethsara <kamesh317nethsara@gmail.com>
2026-08-24 20:12:53 +0530

    Add WebFlux Gateway and JJWT dependencies; update ApiGatewayApplication

--- src/main/java/com/unstray/platform/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/unstray/platform/api_gateway/ApiGatewayApplication.java
@@ -2,10 +2,9 @@ package com.unstray.platform.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
@@ -13,3 +12,5 @@ public class ApiGatewayApplication {
 	}
 
 }
+
+
